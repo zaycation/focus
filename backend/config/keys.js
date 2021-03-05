d584b1fec9d6a6c810dfb85d2683bd6b555c61d5
zaycation.dev <isaiahthomas098@gmail.com>
2021-03-05 11:38:18 -0800

    setup jwt auth for login/signup within the backend

---
+++ backend/config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://zaycation:100Oranges@focus-app.3tqic.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  secretOrKey: "secret",
+};
--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,39 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+const users = require("./routes/api/users");
 const blogPosts = require("./data/blogPosts");
 
 const app = express();
 
+// Bodyparser middleware
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
+app.use(bodyParser.json());
+
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+// Connect to MongoDB
+mongoose
+  .connect(db, { useNewUrlParser: true })
+  .then(() => console.log("MongoDB successfully connected"))
+  .catch((err) => console.log(err));
+
+// Passport middleware
+app.use(passport.initialize());
+
+// Passport config
+require("./config/passport")(passport);
+
+// Routes
+app.use("/api/users", users);
+
+// Get methods
 app.get("/", (req, res) => {
   res.send("API is running...");
 });
@@ -16,4 +47,6 @@ app.get("/api/posts/:id", (req, res) => {
   res.json(blogPost);
 });
 
-app.listen(5000, console.log("Server running on port 5k"));
+const port = process.env.PORT || 5000; // process.env.port is Heroku's port if you choose to deploy the app there
+
+app.listen(port, () => console.log(`Server live on port ${port}!`));
